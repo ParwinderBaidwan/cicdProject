026de29611f9cefef3b6fc5526039c8aa08a6213
ParwinderBaidwan <parwinderb550@gmail.com>
2024-04-30 15:06:09 +0530

    modify app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// create a new repo
+// add .readme .gitignore files
+// copy the url
+// create a new folder on your machine
+// git clone https://github.com/ParwinderBaidwan/cicdProject.git
+// open the project in code (cd foldername)
+// npm init -y
+// add new file app.js with some console statements
+// npm i
+// git status
+// git add .
+// git commit -m "Message to add"
+// git push
+
+console.log("app is running");
+console.log(new Date());
